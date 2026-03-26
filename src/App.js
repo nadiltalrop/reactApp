0f844008e5a7420c95cc6a02e18c2bb9cd69116f
nadil.talrop <nadil.talrop@gmail.com>
2026-03-26 21:58:19 +0530

    paragraph added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
+import { Container } from 'postcss';
 import './App.css';
 import StyleSheet from 'styled-components';
 
 function App() {
   return (
-    	<>
+    	<ContainerDiv>
 			<Headding>Welcome to my FirstReact App</Headding>
-		</>
+			<Paragraph>This is a simple paragraph. "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</Paragraph>
+		</ContainerDiv>
 	);
 }	
 
-const Headding = StyleSheet.h1`
+const ContainerDiv = StyleSheet.div`
 	display: flex;
-	justify-content: center;
+	flex-direction: column;
 	align-items: center;
-	margin : 100px;
+	justify-content: center;
+	margin: auto;
+	background-color: #f0f0f0;
+	width: 100%;
+	height: 100vh;
 `;
 
+const Headding = StyleSheet.h1`
+	
+`;
+
+const Paragraph = StyleSheet.p`
+	padding: 20px;
+`;
 export default App;
